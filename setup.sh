de80c417d141c270be344569035ca882bd4757e2
Ykoh <ykoh@student.42seoul.kr>
2021-02-05 21:36:41 +0900

    Add: setup.sh

---
+++ setup.sh
@@ -0,0 +1,78 @@
+./delete.sh
+
+# MetalLB for docker desktop
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/MetalLB/config.yaml
+
+
+# Nginx
+docker build -t nginx srcs/Nginx
+# docker run -p 80:80 -p 443:443 -p 22:22 -d nginx
+kubectl apply -f srcs/Nginx/config.yaml
+
+# WordPress
+docker build -t wordpress srcs/WordPress
+# # docker run -p 5050:5050 -it wordpress
+kubectl apply -f srcs/WordPress/config.yaml
+
+# docker build -t phpmyadmin srcs/PhpMyAdmin
+# # docker run -p 5000:5000 -it phpmyadmin
+# kubectl apply -f srcs/phpmyadmin/config.yaml
+
+# # docker build -t mysql srcs/MySQL
+# # docker run -p 3306:3306 -it mysql
+
+# # docker build -t ftps srcs/FTPS
+# # docker run -p 20:20 -p 21:21 -p 20020:20020 -p 20021:20021 -it ftps
+
+# # docker build -t grafana srcs/Grafana
+# # docker run -p 3000:3000 -it grafana
+
+# # docker build -t influxdb srcs/influxDB
+# # docker run -p 8086:8086 -it influxdb
+
+
+
+
+
+
+# # ############################################################################
+# # # Dashboard
+# # kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+
+# # # Creating sample user
+
+# # # Creating a Service Account
+# # cat <<EOF | kubectl apply -f -
+# # apiVersion: v1
+# # kind: ServiceAccount
+# # metadata:
+# #   name: admin-user
+# #   namespace: kubernetes-dashboard
+# # EOF
+
+# # # Creating a ClusterRoleBinding
+# # cat <<EOF | kubectl apply -f -
+# # apiVersion: rbac.authorization.k8s.io/v1
+# # kind: ClusterRoleBinding
+# # metadata:
+# #   name: admin-user
+# # roleRef:
+# #   apiGroup: rbac.authorization.k8s.io
+# #   kind: ClusterRole
+# #   name: cluster-admin
+# # subjects:
+# # - kind: ServiceAccount
+# #   name: admin-user
+# #   namespace: kubernetes-dashboard
+# # EOF
+
+# # # Getting a Bearer Token
+# # kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
+
+# # kubectl proxy &
+
+# # open "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/overview?namespace=default"
+# # ############################################################################
