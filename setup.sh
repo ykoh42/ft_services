4c9a9ba543bfea0652be86d5811fe7b3070b99ea
Ykoh <ykoh@student.42seoul.kr>
2021-02-13 04:24:08 +0900

    Fix: setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,128 @@
+#########################################################################################################################
 # Delete image
+#########################################################################################################################
 # ./delete.sh
+#########################################################################################################################
 
+
+#########################################################################################################################
 # Secret
+#########################################################################################################################
 kubectl create secret generic host-ip --from-literal=HOST_IP="$(ipconfig getifaddr en0)"
 kubectl create secret generic account --from-literal=USER="ykoh" --from-literal=PASSWORD="ykoh"
+#########################################################################################################################
 
+
+
+#########################################################################################################################
+# MetalLB
+#########################################################################################################################
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+export ADDRESSES=`ipconfig getifaddr en0`-`ipconfig getifaddr en0`
+sed -i "" "s/ADDRESSES/$ADDRESSES/" srcs/MetalLB/config.yaml
+kubectl apply -f srcs/MetalLB/config.yaml
+sed -i "" "s/$ADDRESSES/ADDRESSES/" srcs/MetalLB/config.yaml
+#########################################################################################################################
+
+
+
+#########################################################################################################################
+# MySQL
+#########################################################################################################################
+# docker build -t mysql srcs/MySQL && docker run -p 3306:3306 -it mysql
+kubectl delete -f srcs/MySQL/config.yaml
+docker build -t mysql srcs/MySQL
+kubectl apply -f srcs/MySQL/config.yaml
+#########################################################################################################################
+
+
+
+#########################################################################################################################
+# PhpMyAdmin
+#########################################################################################################################
+# docker build -t phpmyadmin srcs/PhpMyAdmin && docker run -p 5000:5000 -it phpmyadmin
+kubectl delete -f srcs/PhpMyAdmin/config.yaml
+docker build -t phpmyadmin srcs/PhpMyAdmin
+kubectl apply -f srcs/PhpMyAdmin/config.yaml
+#########################################################################################################################
+
+
+
+#########################################################################################################################
+# WordPress
+#########################################################################################################################
+# docker build -t wordpress srcs/WordPress && docker run -p 5050:5050 -it wordpress
+kubectl delete -f srcs/WordPress/config.yaml
+docker build -t wordpress srcs/WordPress
+kubectl apply -f srcs/WordPress/config.yaml
+#########################################################################################################################
+
+
+
+#########################################################################################################################
+# Nginx
+#########################################################################################################################
+# docker build -t nginx srcs/Nginx && docker run -p 22:22 -it nginx
+kubectl delete -f srcs/Nginx/config.yaml
+docker build -t nginx srcs/Nginx
+kubectl apply -f srcs/Nginx/config.yaml
+#########################################################################################################################
+
+
+
+#########################################################################################################################
+# telegraf
+#########################################################################################################################
+# docker build -t telegraf srcs/telegraf && docker run -it telegraf
+kubectl delete -f srcs/telegraf/config.yaml
+docker build -t telegraf srcs/telegraf
+kubectl apply -f srcs/telegraf/config.yaml
+#########################################################################################################################
+
+
+
+
+#########################################################################################################################
+# influxDB
+#########################################################################################################################
+# docker build -t influxdb srcs/influxDB && docker run -p 8086:8086 -it influxdb
+kubectl delete -f srcs/influxDB/config.yaml
+docker build -t influxdb srcs/influxDB
+kubectl apply -f srcs/influxDB/config.yaml
+#########################################################################################################################
+
+
+
+#########################################################################################################################
+# Grafana
+#########################################################################################################################
+# docker build -t grafana srcs/Grafana && docker run -p 3000:3000 -it grafana
+kubectl delete -f srcs/Grafana/config.yaml
+docker build -t grafana srcs/Grafana
+kubectl apply -f srcs/Grafana/config.yaml
+#########################################################################################################################
+
+
+
+#########################################################################################################################
+# FTPS
+#########################################################################################################################
+kubectl delete -f srcs/FTPS/config.yaml
+docker build -t ftps srcs/FTPS
+export HOST_IP=`ipconfig getifaddr en0`
+sed -i "" "s/HOST_IP/$HOST_IP/" srcs/FTPS/config.yaml
+kubectl apply -f srcs/FTPS/config.yaml
+sed -i "" "s/$HOST_IP/HOST_IP/" srcs/FTPS/config.yaml
+#########################################################################################################################
+
+
+
+#########################################################################################################################
 # Dashboard
+#########################################################################################################################
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
-
-# Creating sample user
 
 # Creating a Service Account
 cat <<EOF | kubectl apply -f -
@@ -35,52 +149,14 @@ subjects:
   namespace: kubernetes-dashboard
 EOF
 
-kubectl proxy &
-
-
-# MetalLB
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-export ADDRESSES=`ipconfig getifaddr en0`-`ipconfig getifaddr en0`
-sed -i "" "s/ADDRESSES/$ADDRESSES/" srcs/MetalLB/config.yaml
-kubectl apply -f srcs/MetalLB/config.yaml
-sed -i "" "s/$ADDRESSES/ADDRESSES/" srcs/MetalLB/config.yaml
-
-
-# MySQL
-kubectl delete -f srcs/MySQL/config.yaml
-docker build -t mysql srcs/MySQL
-kubectl apply -f srcs/MySQL/config.yaml
-
-# PhpMyAdmin
-kubectl delete -f srcs/phpmyadmin/config.yaml
-docker build -t phpmyadmin srcs/PhpMyAdmin
-kubectl apply -f srcs/phpmyadmin/config.yaml
-
-# WordPress
-# docker build -t wordpress srcs/WordPress && docker run -p 5050:5050 -it wordpress
-kubectl delete -f srcs/WordPress/config.yaml
-docker build -t wordpress srcs/WordPress
-kubectl apply -f srcs/WordPress/config.yaml
-
-# Nginx
-kubectl delete -f srcs/Nginx/config.yaml
-docker build -t nginx srcs/Nginx
-kubectl apply -f srcs/Nginx/config.yaml
-
-
-
-
 # Getting a Bearer Token
-printf "Bearer Token\n\n"
+echo "Bearer Token\n"
 kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
-printf "\n"
+echo "\n"
 
-sleep 3
+# Dashboard URL
+echo open "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
 
-# echo "open following URL"
-# echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/overview?namespace=default"
-open "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/overview?namespace=default"
-############################################################################
-
+# Commandline proxy
+kubectl proxy
+#########################################################################################################################
