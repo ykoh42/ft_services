155fdf507653ab5f103031bc9b67a919909b4429
Ykoh <ykoh@student.42seoul.kr>
2021-02-18 00:37:24 +0900

    Rm :ssh

--- srcs/Nginx/entry.sh
+++ srcs/Nginx/entry.sh
@@ -7,11 +7,11 @@ openssl req \
 -out /etc/ssl/certs/services.crt \
 -subj "/C=KR/ST=SEOUL/O=42SEOUL/CN=$HOST_IP"
 
-ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+# ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+# ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
 
-mkdir -p /var/run/sshd
-/usr/sbin/sshd
+# mkdir -p /var/run/sshd
+# /usr/sbin/sshd
 
 mkdir -p /run/nginx
 nginx -g "daemon off;"
